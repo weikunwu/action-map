7079b2e7b58b20751604156bdb2fbbbe4150a99a
Weikun Wu <wwu1997@berkeley.edu>
2020-08-10 12:47:01 +0000

    [feat] Added new columns for representatives and store the info in the representative model

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,10 +2,36 @@
 
 class Representative < ApplicationRecord
     has_many :news_items, dependent: :delete_all
-
+    
+    def address
+        {:line1=>self.address_line1, :line2=>self.address_line2,
+         :line3=>self.address_line3, :city=>self.city,
+         :state=>self.state, :zip=>self.zip}
+    end
+    
+    class FakeAddress
+        def line1
+            return nil
+        end
+        def line2
+            return nil
+        end
+        def line3
+            return nil
+        end        
+        def city
+            return nil
+        end        
+        def state
+            return nil
+        end        
+        def zip
+            return nil
+        end
+    end
+  
     def self.civic_api_to_representative_params(rep_info)
         reps = []
-
         rep_info.officials.each_with_index do |official, index|
             ocdid_temp = ''
             title_temp = ''
@@ -19,6 +45,23 @@ class Representative < ApplicationRecord
 
             rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
                                            title: title_temp })
+            
+            if official.address.nil?
+              address = FakeAddress.new
+            else
+              address = official.address[0]
+            end
+          
+            rep.address_line1 = address.line1
+            rep.address_line2 = address.line2
+            rep.address_line3 = address.line3
+            rep.city = address.city
+            rep.state = address.state
+            rep.zip = address.zip
+          
+            rep.party = official.party
+            rep.photourl = official.photo_url
+
             reps.push(rep)
         end
 
