5085700eb52fa7d5265bc024d62e3470b9dca152
Ruhang Ma <m.ruhang@berkeley.edu>
2020-08-12 05:49:53 +0000

    [feat] Rating can be saved and accessed

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -4,6 +4,7 @@ class MyNewsItemsController < SessionController
     before_action :set_representative
     before_action :set_representatives_list
     before_action :set_news_item, only: %i[edit update destroy]
+    before_action :set_rating, only: %i[edit update destroy]
 
     def new
         @news_item = NewsItem.new
@@ -16,19 +17,27 @@ class MyNewsItemsController < SessionController
     end
 
     def create
-#         @news_item = NewsItem.new(news_item_params)
         @news_item = @representative.news_items.new(news_item_params)
-        puts "*************************"
-        puts "DEBUG received rating:", news_item_params[:rating]
         if @news_item.save
             redirect_to representative_news_item_path(@representative, @news_item),
                         notice: 'News item was successfully created.'
         else
             render :new, error: 'An error occurred when creating the news item.'
         end
+        
+#         @rating = @news_item.ratings.create(score: rating_params[:score], user_id: current_user.id)
+        @rating = Rating.create!(score: rating_params[:score], user_id: @current_user.id, news_item_id: @news_item.id)
+        
     end
 
     def update
+        if @rating == nil
+            @rating = Rating.create(score: rating_params[:score], user_id: @current_user.id, news_item_id: @news_item.id)
+        elsif @rating.update(score: rating_params[:score])
+        else
+            render :edit, error: 'An error occurred when updating the rating.'
+        end
+        
         if @news_item.update(news_item_params)
             redirect_to representative_news_item_path(@representative, @news_item),
                         notice: 'News item was successfully updated.'
@@ -38,6 +47,8 @@ class MyNewsItemsController < SessionController
     end
 
     def destroy
+        puts "*********************"
+        puts "DEBUG: destroy called!"
         @news_item.destroy
         redirect_to representative_news_items_path(@representative),
                     notice: 'News was successfully destroyed.'
@@ -58,9 +69,16 @@ class MyNewsItemsController < SessionController
     def set_news_item
         @news_item = NewsItem.find(params[:id])
     end
+    
+    def set_rating
+        @rating = @news_item.ratings.find_by user_id: @current_user.id
+    end
 
     # Only allow a list of trusted parameters through.
     def news_item_params
         params.require(:news_item).permit(:news, :title, :description, :link, :representative_id, :issue, :rating)
+    end
+    def rating_params
+        params.require(:rating).permit(:score)
     end
 end
