3555daf976981207d1ee45cbf4f7f87d64e69163
Weikun Wu <wwu1997@berkeley.edu>
2020-08-13 06:03:21 +0000

    [fix] Fixed features linter problem

--- features/step_definitions/representative_steps.rb
+++ features/step_definitions/representative_steps.rb
@@ -1,54 +1,52 @@
+# frozen_string_literal: true
 
-
-
-Given /the following representatives exist/ do |representatives_table|
+Given(/the following representatives exist/) do |representatives_table|
     representatives_table.hashes.each do |representative|
-      Representative.create representative
+        Representative.create representative
     end
-  end
+end
 
-Given /^(?:|I )am on (.+)$/ do |page_name|
+Given(/^(?:|I )am on (.+)$/) do |page_name|
     visit path_to(page_name)
 end
 
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-    fill_in(field, :with => value)
+When(/^(?:|I )fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
+    fill_in(field, with: value)
 end
 
-When /^(?:|I )press '([^"]*)'$/ do |button|
+When(/^(?:|I )press '([^"]*)'$/) do |button|
     click_button(button)
 end
 
-When("I follow {string}") do |string|
+When('I follow {string}') do |string|
     # puts page.body
-    link = string + "Donald J. Trump"
+    link = string + 'Donald J. Trump'
     click_link(link)
 end
 
-Then("I should see {string}") do |string|
+Then('I should see {string}') do |string|
     if page.respond_to? :should
-        page.should have_content(text)
+        page.should have_content(string)
     else
-        assert page.has_content?(text)
+        assert page.has_content?(string)
     end
 end
 
-Then("I should not see {string}") do |string|
+Then('I should not see {string}') do |string|
     if page.respond_to? :should
-        !page.should have_content(text)
+        !page.should have_content(string)
     else
-        !assert page.has_content?(text)
+        !assert page.has_content?(string)
     end
 end
 
 def path_to(page_name)
     case page_name
-    when /search page/i then 
+    when /search page/i
         representatives_path
-    when /profile page/i then
+    when /profile page/i
         representative_profile_page_path(1)
-    when /results page/i then
-        search_representatives_path("california")
+    when /results page/i
+        search_representatives_path('california')
     end
-
 end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module NavigationHelpers
     def path_to(page_name)
         case page_name
-        when /search page/i then 
+        when /search page/i
             search_representatives_path
         end
     end
